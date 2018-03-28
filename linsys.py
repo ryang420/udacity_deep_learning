8c8d51e9609f52ddef1f282ad5759c49179c6dea
David Yang <David.Yang@activenetwork.com>
2018-03-28 16:57:46 +0800

    dev

--- linsys.py
+++ linsys.py
@@ -119,6 +119,7 @@ class LinearSystem(object):
 
         return tf
 
+    # 清除系数，如 2x = 4 转换成 x = 2
     def scale_row(self, row, col):
         n = self[row].normal_vector.coordinates
         beta = Decimal('1.0') / n[col]
@@ -129,6 +130,48 @@ class LinearSystem(object):
             n = self[k].normal_vector.coordinates
             alpha = -(n[col])
             self.add_multiple_times_row_to_row(alpha, row, k)
+
+    # 求解方程组
+    def compute_solution(self):
+        try:
+            return self.do_gaussian_solution()
+        except Exception as e:
+            if (str(e) == self.NO_SOLUTIONS_MSG or
+                    str(e) == self.INF_SOLUTIONS_MSG):
+                return str(e)
+            else:
+                raise e
+
+    def do_gaussian_solution(self):
+        rref = self.compute_rref()
+
+        rref.raise_exception_if_contradictory_equation()
+        rref.raise_exception_if_too_few_pivots()
+
+        num_variables = rref.dimension
+        solution_coordinates = [rref.planes[i].constant_term for i in range(num_variables)]
+
+        return Vector(solution_coordinates)
+
+    def raise_exception_if_contradictory_equation(self):
+        for p in self.planes:
+            try:
+                p.first_nonzero_index(p.normal_vector.coordinates)
+            except Exception as e:
+                if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
+                    constant_term = MyDecimal(p.constant_term)
+                    if not constant_term.is_near_zero():
+                        raise Exception(self.NO_SOLUTIONS_MSG)
+                else:
+                    raise e
+
+    def raise_exception_if_too_few_pivots(self):
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        num_pivots = sum([1 if index >= 0 else 0 for index in pivot_indices])
+        num_variables = self.dimension
+
+        if num_pivots < num_variables:
+            raise Exception(self.INF_SOLUTIONS_MSG)
 
     def __len__(self):
         return len(self.planes)
@@ -253,7 +296,7 @@ if not (t[0] == Plane(normal_vector=Vector(['1', '-1', '1']), constant_term='2')
         t[1] == Plane(normal_vector=Vector(['0', '1', '1']), constant_term='1') and
         t[2] == Plane(normal_vector=Vector(['0', '0', '-9']), constant_term='-2')):
     print('test case 4 failed')
-'''
+
 
 # 编写RREF函数
 p1 = Plane(normal_vector=Vector(['1', '1', '1']), constant_term='1')
@@ -293,3 +336,20 @@ if not (r[0] == Plane(normal_vector=Vector(['1', '0', '0']), constant_term=Decim
         r[1] == Plane(normal_vector=Vector(['0', '1', '0']), constant_term=Decimal('7') / Decimal('9')) and
         r[2] == Plane(normal_vector=Vector(['0', '0', '1']), constant_term=Decimal('2') / Decimal('9'))):
     print('test case 4 failed')
+'''
+
+# p1 = Plane(normal_vector=Vector(['5.862', '1.178', '-10.366']), constant_term='-8.15')
+# p2 = Plane(normal_vector=Vector(['-2.931', '-0.589', '5.183']), constant_term='-4.075')
+# s = LinearSystem([p1, p2])
+
+# p1 = Plane(normal_vector=Vector(['8.631', '5.112', '-1.816']), constant_term='-5.113')
+# p2 = Plane(normal_vector=Vector(['4.315', '11.132', '-5.27']), constant_term='-6.775')
+# p3 = Plane(normal_vector=Vector(['-2.158', '3.01', '-1.727']), constant_term='-0.831')
+# s = LinearSystem([p1, p2, p3])
+
+p1 = Plane(normal_vector=Vector(['5.262', '2.739', '-9.878']), constant_term='-3.441')
+p2 = Plane(normal_vector=Vector(['5.111', '6.358', '7.638']), constant_term='-2.152')
+p3 = Plane(normal_vector=Vector(['2.016', '-9.924', '-1.367']), constant_term='-9.278')
+p4 = Plane(normal_vector=Vector(['2.167', '-13.543', '-18.883']), constant_term='-10.567')
+s = LinearSystem([p1, p2, p3, p4])
+print(s.compute_solution())
